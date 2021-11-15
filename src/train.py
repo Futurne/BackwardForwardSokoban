292f66d93acb5c8610a5476d1da21d0b46128080
Pierre PEREIRA <pierre.pereira@grenoble-inp.com>
2021-11-14 19:20:13 -0500

    feat: update node's values

--- src/train.py
+++ src/train.py
@@ -17,7 +17,10 @@ def train_on_env(model: BaseModel, env: MacroSokobanEnv):
         for child in leaf_node.children:
             child.eval(model)  # Evaluate child's value
 
-        # Find the best child
+        # Find the best child and compute target
         best_child = max(leaf_node.childre, key=lambda node: node.value)
+        target = best_child.torch_reward + best_child.torch_value
         # Compute loss
-        loss = (best_child.reward + best_child.torch_value.detach() - leaf_node.torch_value).pow(2)
+        loss = (target.detach() - leaf_node.torch_value).pow(2)
+        # Update the new value estimate of the node and all its parents
+        leaf_node.update_value(target)
