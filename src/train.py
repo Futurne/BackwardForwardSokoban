7089b21c5e64c18e4c8efe02a4b217d924f71ef2
Pierre PEREIRA <pierre.pereira@grenoble-inp.com>
2021-11-27 18:41:53 -0500

    refacto: simplifying the training loop code

--- src/train.py
+++ src/train.py
@@ -7,6 +7,29 @@ from environments import MacroSokobanEnv
 from search_tree import SearchTree, Node
 
 
+def expand_node(tree: SearchTree, node: Node, model: BaseModel):
+    """Expand the node and update all its children values.
+    """
+    node.expand(tree)  # Expand new nodes
+    with torch.no_grad():
+        for child in node.children:
+            model.estimate(child, gamma=0.9)  # Evaluate child's value
+
+
+def compute_loss(model: BaseModel, node: Node):
+    """Compute the loss for the given node prediction.
+
+    It predicts the node's value, and compare it with its children.
+    The node must already have children with updated values!
+    """
+    # Find the best child and compute loss
+    best_child = max(node.children, key=lambda node: node.value)
+    prediction = model.estimate(node, gamma=0.9)  # Shape [1, 1]
+    target = torch.FloatTensor([[best_child.value + best_child.reward]])  # Shape [1, 1]
+    loss = (target - prediction).pow(2).sum()
+    return loss
+
+
 def forward_train(model: BaseModel, env: MacroSokobanEnv, config: dict):
     """Example of a training loop on a single environment.
     """
@@ -27,17 +50,10 @@ def forward_train(model: BaseModel, env: MacroSokobanEnv, config: dict):
         board, player = build_board_from_raw(raw)
         print_board(board, player)
 
-        leaf_node.expand(tree)  # Expand new nodes
-        with torch.no_grad():
-            for child in leaf_node.children:
-                model.estimate(child, gamma)  # Evaluate child's value
+        expand_node(tree, leaf_node, model)
 
         if leaf_node.children:
-            # Find the best child and compute loss
-            best_child = max(leaf_node.children, key=lambda node: node.value)
-            prediction = model.estimate(leaf_node, gamma)  # Shape [1, 1]
-            target = torch.FloatTensor([[best_child.value + best_child.reward]])  # Shape [1, 1]
-            loss = (target - prediction).pow(2).sum()
+            loss = compute_loss(model, leaf_node)
 
             # Update model
             optimizer.zero_grad()
@@ -51,8 +67,9 @@ def forward_train(model: BaseModel, env: MacroSokobanEnv, config: dict):
 if __name__ == '__main__':
     from models import LinearModel
     from utils import core_feature, print_board, build_board_from_raw
+    from environments import BackwardSokobanEnv
 
-    env = MacroSokobanEnv(dim_room=(10, 10), num_boxes=3)
+    env = BackwardSokobanEnv(dim_room=(10, 10), num_boxes=3)
     feat = core_feature(env, 0.9)
     model = LinearModel(len(feat))
 
